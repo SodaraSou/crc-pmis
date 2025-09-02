1eae5b10ab65a4b69db8378a03ad63df4a901bed
sodarasou <sodarasou168@gmail.com>
2025-09-02 16:13:34 +0700

    fix style

--- resources/views/branch/branch-show.blade.php
+++ resources/views/branch/branch-show.blade.php
@@ -6,8 +6,50 @@
 
 @section('content')
     <section class="container">
-        <livewire:branch.branch-employee-table :branch="$branch" />
-        <livewire:sub-branch.sub-branch-table :branch="$branch" />
+        <div class="card card-danger card-outline card-outline-tabs">
+            <div class="card-header p-0 border-bottom-0">
+                <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="custom-tabs-four-home-tab" data-toggle="pill"
+                            href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home"
+                            aria-selected="false">គណ:កិត្តិយស</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-profile-tab" data-toggle="pill"
+                            href="#custom-tabs-four-profile" role="tab" aria-controls="custom-tabs-four-profile"
+                            aria-selected="true">គណ:កម្មាធិការ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill"
+                            href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages"
+                            aria-selected="false">មន្ត្រីប្រតិបត្តិ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-settings-tab" data-toggle="pill"
+                            href="#custom-tabs-four-settings" role="tab" aria-controls="custom-tabs-four-settings"
+                            aria-selected="false">អនុសាខា</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <div class="tab-content" id="custom-tabs-four-tabContent">
+                    <div class="tab-pane fade active show" id="custom-tabs-four-home" role="tabpanel"
+                        aria-labelledby="custom-tabs-four-home-tab">
+                        <livewire:branch.branch-employee-table :branch="$branch" />
+                    </div>
+                    <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel"
+                        aria-labelledby="custom-tabs-four-profile-tab">
+                    </div>
+                    <div class="tab-pane fade" id="custom-tabs-four-messages" role="tabpanel"
+                        aria-labelledby="custom-tabs-four-messages-tab">
+                    </div>
+                    <div class="tab-pane fade" id="custom-tabs-four-settings" role="tabpanel"
+                        aria-labelledby="custom-tabs-four-settings-tab">
+                        <livewire:sub-branch.sub-branch-table :branch="$branch" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
 
