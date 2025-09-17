815988e6585cb74ead999bc9df604bfcb5cce0d2
Sodara Sou <sodarasou168@gmail.com>
2025-09-17 23:32:24 +0700

    other change

--- app/Livewire/HonoraryCommittee/HonoraryCommitteeMemberTable.php
+++ app/Livewire/HonoraryCommittee/HonoraryCommitteeMemberTable.php
@@ -2,9 +2,14 @@
 
 namespace App\Livewire\HonoraryCommittee;
 
+use App\Models\BranchTerm;
+use App\Models\Committee;
 use App\Models\CommitteeLevel;
+use App\Models\CommitteeMember;
 use App\Models\Member;
+use App\Models\SubBranchTerm;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -15,12 +20,24 @@ class HonoraryCommitteeMemberTable extends Component
 
     public $user = null;
 
-    #[Url()]
+    #[Url(except: '')]
     public $search = "";
 
     public $active_term = false;
 
-    public $committee_level_id = null;
+    #[Url(except: '')]
+    public $committee_level_id = '';
+    public $filter_committee_level = null;
+
+    public $committees = [];
+    #[Url(except: '')]
+    public $committee_id = '';
+    public $filter_committee = null;
+
+    public $terms = [];
+    #[Url(except: '')]
+    public $term_id = '';
+    public $filter_term = null;
 
     public $per_page = 25;
 
@@ -29,18 +46,116 @@ class HonoraryCommitteeMemberTable extends Component
         $this->user = Auth::user();
     }
 
+    public function resetFilter(): void
+    {
+        $this->reset('search', 'committee_level_id', 'committee_id', 'term_id');
+        $this->per_page = 25;
+        $this->filter_committee_level = '';
+        $this->filter_committee = '';
+        $this->filter_term = '';
+    }
+
+    public function removeFilter($filter): void
+    {
+        if ($filter == 'committee_level') {
+            $this->committee_level_id = '';
+            $this->filter_committee_level = null;
+        } elseif ($filter == 'committee') {
+            $this->committee_id = '';
+            $this->filter_committee = null;
+        } elseif ($filter == 'term') {
+            $this->term_id = '';
+            $this->filter_term = null;
+        }
+    }
+
+    public function updatedCommitteeLevelId()
+    {
+        $this->committees = Committee::where('active', true)
+            ->where('committee_level_id', $this->committee_level_id)
+            ->get();
+        $this->filter_committee_level = CommitteeLevel::find($this->committee_level_id);
+    }
+
+    public function updatedCommitteeId()
+    {
+        $committee = Committee::where('active', true)
+            ->where('id', $this->committee_id)
+            ->first();
+        $this->filter_committee = $committee;
+        if ($this->committee_level_id == 1) {
+            $this->terms = BranchTerm::where('active', true)
+                ->where('branch_id', $committee->branch->id)
+                ->get();
+        } elseif ($this->committee_level_id == 2) {
+            $this->terms = SubBranchTerm::where('active', true)
+                ->where('sub_branch_id', $committee->sub_branch->id)
+                ->get();
+        }
+    }
+
+    #[On('confirmed_delete')]
+    public function delete($member_id)
+    {
+        try {
+            $member = Member::where('id', $member_id)->first();
+
+            $member->update([
+                'active' => false,
+                'updated_by' => $this->user->id
+            ]);
+
+            CommitteeMember::where('active', true)
+                ->where('member_id', $member_id)
+                ->update([
+                    'active' => false,
+                    'updated_by' => $this->user->id
+                ]);
+
+            $this->dispatch('delete_success');
+        } catch (\Exception $e) {
+            $this->dispatch('delete_fail', message: $e->getMessage());
+        }
+    }
+
     public function render()
     {
-        $today = now()->toDateString();
+        $query = Member::query();
 
-        $members = Member::where('active', true)
+        $query->where('active', true)
             ->whereHas('committees', function ($q) {
                 $q->where('committee_type_id', 1);
-            })
-            ->paginate($this->per_page);
+            });
+
+        if ($this->search) {
+            $query->where('kh_name', 'like', '%' . $this->search . '%');
+        }
+
+        if ($this->committee_id) {
+            $query->whereHas('committee_members', function ($cm) {
+                $cm->where('active', true)
+                    ->where('committee_id', $this->committee_id);
+            });
+        }
+
+        if ($this->term_id) {
+            if ($this->committee_level_id == 1) {
+                $query->whereHas('committee_members', function ($cm) {
+                    $cm->where('active', true)
+                        ->where('branch_term_id', $this->term_id);
+                });
+                $this->filter_term = BranchTerm::find($this->term_id);
+            } elseif ($this->committee_level_id == 2) {
+                $query->whereHas('committee_members', function ($cm) {
+                    $cm->where('active', true)
+                        ->where('sub_branch_term_id', $this->term_id);
+                });
+                $this->filter_term = SubBranchTerm::find($this->term_id);
+            }
+        }
 
         return view('livewire.honorary-committee.honorary-committee-member-table', [
-            'members' => $members,
+            'members' => $query->paginate($this->per_page),
             'committee_levels' => CommitteeLevel::all(),
         ]);
     }
--- resources/views/livewire/honorary-committee/honorary-committee-member-table.blade.php
+++ resources/views/livewire/honorary-committee/honorary-committee-member-table.blade.php
@@ -26,28 +26,171 @@
                     </select>
                 </div>
                 <div class="col-12 col-md-3 form-group">
-                    <label></label>
-                    <select wire:model.live="committee_level_id" class="form-control">
-                        <option value="">ជ្រើសរើសថ្នាក់គណ:កិត្តិយស</option>
-                        @foreach ($committee_levels as $committee_level)
-                            <option wire:key="{{ $committee_level->id }}" value="{{ $committee_level->id }}">
-                                {{ $committee_level->kh_name }}
+                    <label>គណ:កិត្តិយស</label>
+                    <select wire:model.live="committee_id" class="form-control">
+                        <option value="">ជ្រើសរើសគណ:កិត្តិយស</option>
+                        @foreach ($committees as $committee)
+                            <option wire:key="{{ $committee->id }}" value="{{ $committee->id }}">
+                                {{ $committee->kh_name }}
                             </option>
                         @endforeach
                     </select>
                 </div>
-                {{-- <div class="col-12 col-md-3 form-group">
+                <div class="col-12 col-md-3 form-group">
+                    <label>អាណត្តិ</label>
+                    <select wire:model.live="term_id" class="form-control">
+                        <option value="">ជ្រើសរើសអាណត្តិ</option>
+                        @foreach ($terms as $term)
+                            <option wire:key="{{ $term->id }}" value="{{ $term->id }}">
+                                {{ $term->kh_name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-12 col-md-3 form-group">
                     <label>ចំនួនមួយទំព័រ</label>
                     <select wire:model.live='per_page' class="form-control">
                         <option value="25">25</option>
                         <option value="50">50</option>
                         <option value="75">75</option>
                     </select>
-                </div> --}}
+                </div>
+            </div>
+            <div class="d-flex align-items-center">
+                @if ($committee_level_id || $committee_id || $term_id)
+                    <span class="mr-1">Applied Filters:</span>
+                    @if ($committee_level_id)
+                        <span wire:key="filter-pill-gender"
+                            class="badge badge-pill badge-info d-inline-flex align-items-center mr-2">
+                            នាយកដ្ឋាន: {{ $filter_committee_level->kh_name }}
+                            <a href="#" wire:click.prevent="removeFilter('committee_level')"
+                                class="text-white ml-2">
+                                <span class="sr-only">Remove filter option</span>
+                                <svg style="width:.5em;height:.5em" stroke="currentColor" fill="none"
+                                    viewBox="0 0 8 8">
+                                    <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7"></path>
+                                </svg>
+                            </a>
+                        </span>
+                    @endif
+                    @if ($committee_id)
+                        <span wire:key="filter-pill-gender"
+                            class="badge badge-pill badge-info d-inline-flex align-items-center mr-2">
+                            សាខា: {{ $filter_committee->kh_name }}
+                            <a href="#" wire:click.prevent="removeFilter('committee')" class="text-white ml-2">
+                                <span class="sr-only">Remove filter option</span>
+                                <svg style="width:.5em;height:.5em" stroke="currentColor" fill="none"
+                                    viewBox="0 0 8 8">
+                                    <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7"></path>
+                                </svg>
+                            </a>
+                        </span>
+                    @endif
+                    @if ($term_id)
+                        <span wire:key="filter-pill-gender"
+                            class="badge badge-pill badge-info d-inline-flex align-items-center mr-2">
+                            សាខា: {{ $filter_term->kh_name }}
+                            <a href="#" wire:click.prevent="removeFilter('committee')" class="text-white ml-2">
+                                <span class="sr-only">Remove filter option</span>
+                                <svg style="width:.5em;height:.5em" stroke="currentColor" fill="none"
+                                    viewBox="0 0 8 8">
+                                    <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7"></path>
+                                </svg>
+                            </a>
+                        </span>
+                    @endif
+                    <a href="#" wire:click.prevent="resetFilter">Clear</a>
+                @endif
             </div>
         </div>
         <div class="card-body">
-            <table class="table table-hover text-nowrap">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>ល.រ</th>
+                        <th>ឈ្មោះខ្មែរ</th>
+                        <th>អាណត្តិ</th>
+                        <th>សកម្មភាព</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($members as $member)
+                        <tr wire:key='{{ $member->id }}' aria-expanded="false">
+                            <td>{{ $member->id }}</td>
+                            <td>{{ $member->kh_name }}</td>
+                            <td>
+                                <button class="btn btn-sm btn-success" data-widget="expandable-table"><i
+                                        class="fa fa-eye" aria-hidden="true"></i></button>
+                            </td>
+                            {{-- @php
+                                $committee_membership = $member->committees->first();
+                            @endphp
+                            <td>{{ $committee_membership->branch->kh_name }}</td>
+                            <td>{{ $committee_membership->pivot->gov_position }}</td>
+                            <td>{{ $committee_membership->pivot->committee_position->kh_name }}</td> --}}
+                            <td>
+                                <a href="{{ route('honorary-committee-member.show', $member->id) }}"
+                                    class="btn btn-sm btn-primary mr-2">
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                </a>
+                                <a href="{{ route('honorary-committee-member.edit', $member->id) }}"
+                                    class="btn btn-sm btn-info mr-2">
+                                    <i class="fa fa-pen" aria-hidden="true"></i>
+                                </a>
+                                <button class="btn btn-sm btn-danger"
+                                    wire:click="$dispatch('alert_delete', {member_id: {{ $member->id }}})">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr class="expandable-body d-none">
+                            <td colspan="4">
+                                <div style="display: none;">
+                                    <div class="timeline timeline-inverse">
+                                        @php
+                                            $terms = $member->committee_members()->where('active', true)->get();
+                                        @endphp
+                                        @foreach ($terms as $term)
+                                            <div class="time-label">
+                                                @if ($term->committee->committee_level_id == 1)
+                                                    <span class="bg-success">
+                                                        @if ($term->committee->committee_level_id == 1)
+                                                            {{ $term->branch_term->kh_name }}
+                                                        @elseif ($term->committee->committee_level_id == 2)
+                                                            {{ $term->sub_branch_term->kh_name }}
+                                                        @endif
+                                                    </span>
+                                                @endif
+                                            </div>
+                                            <div>
+                                                <i class="fas fa-suitcase bg-success"></i>
+                                                <div class="timeline-item">
+                                                    <h3 class="timeline-header">{{ $term->committee->kh_name }}</h3>
+                                                    <div class="timeline-body">
+                                                        <div>
+                                                            រយ:ពេល: {{ $term->branch_term->start_date }} ដល់
+                                                            {{ $term->branch_term->end_date }}
+                                                        </div>
+                                                        <div>
+                                                            តួនាទី: {{ $term->committee_position->kh_name }}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                        @if ($terms->count() > 0)
+                                            <div>
+                                                <i class="far fa-clock bg-gray"></i>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{-- <table class="table table-hover text-nowrap">
                 <thead>
                     <tr>
                         <th>ល.រ</th>
@@ -67,8 +210,8 @@
                                 $committee_membership = $member->committees->first();
                             @endphp
                             <td>{{ $committee_membership->branch->kh_name }}</td>
-                            <td>{{ $committee_membership->pivot->committee_position->kh_name }}</td>
                             <td>{{ $committee_membership->pivot->gov_position }}</td>
+                            <td>{{ $committee_membership->pivot->committee_position->kh_name }}</td>
                             <td>
                                 <div class="d-flex justify-content-center align-items-center">
                                     <a href="{{ route('honorary-committee-member.show', $member->id) }}"
@@ -79,13 +222,58 @@
                                         class="btn btn-sm btn-info mr-2">
                                         <i class="fa fa-pen" aria-hidden="true"></i>
                                     </a>
+                                    <button class="btn btn-sm btn-danger"
+                                        wire:click="$dispatch('alert_delete', {member_id: {{ $member->id }}})">
+                                        <i class="fa fa-trash" aria-hidden="true"></i>
+                                    </button>
                                 </div>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
-            </table>
+            </table> --}}
         </div>
         <div class="card-footer">{{ $members->links() }}</div>
     </div>
 </div>
+
+@script
+    <script>
+        $wire.on("alert_delete", (event) => {
+            Swal.fire({
+                title: "តើអ្នកប្រាកដមែនទេ?",
+                text: "សកម្មភាពនេះមិនអាចត្រឡប់ក្រោយបានទេ!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#007bff",
+                cancelButtonColor: "#dc3545",
+                confirmButtonText: "យល់ព្រម",
+                cancelButtonText: "បញ្ឈប់"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $wire.dispatch('confirmed_delete', {
+                        member_id: event.member_id
+                    });
+                }
+            });
+        });
+        $wire.on("delete_success", () => {
+            Swal.fire({
+                title: "ជោគជ័យ",
+                text: "លុបសមាជិកជោគជ័យ",
+                icon: "success",
+                confirmButtonText: "អូខេ",
+                confirmButtonColor: "#28a745"
+            });
+        });
+        $wire.on("delete_fail", (event) => {
+            Swal.fire({
+                title: "មានបញ្ហា!",
+                text: event.message,
+                icon: "error",
+                confirmButtonText: "អូខេ",
+                confirmButtonColor: "#dc3545"
+            });
+        });
+    </script>
+@endscript
