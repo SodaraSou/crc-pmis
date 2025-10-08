b2fd2c51d5f38f7cf8fa571dcdd1400453cdf6dd
sodarasou <sodarasou168@gmail.com>
2025-10-08 14:11:06 +0700

    minor change

--- app/Http/Controllers/HqReportController.php
+++ app/Http/Controllers/HqReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Department;
+use Illuminate\Support\Facades\DB;
 
 class HqReportController extends Controller
 {
@@ -31,7 +32,46 @@ class HqReportController extends Controller
 
     public function honoraryCommitteeReport()
     {
-        return view('hq-report.hq-report-honorary-committee');
+        $test = DB::table('committees')
+            ->where('committee_type_id', 1)
+            ->select('id', 'kh_name')
+            ->get()
+            ->map(function ($committee) {
+                $members = DB::table('committee_member')
+                    ->join('members', 'members.id', '=', 'committee_member.member_id')
+                    ->leftJoin('branch_terms', 'branch_terms.id', '=', 'committee_member.branch_term_id')
+                    ->leftJoin('sub_branch_terms', 'sub_branch_terms.id', '=', 'committee_member.sub_branch_term_id')
+                    ->where('committee_member.committee_id', $committee->id)
+                    ->where('committee_member.active', true)
+                    ->where('members.active', true)
+                    ->where(function ($query) {
+                        $query->where(function ($q) {
+                            $q->where('branch_terms.active', true)
+                                ->where('branch_terms.start_date', '<=', now()->toDateString())
+                                ->whereNull('branch_terms.end_date');
+                        })
+                            ->orWhere(function ($q) {
+                                $q->where('sub_branch_terms.active', true)
+                                    ->where('sub_branch_terms.start_date', '<=', now()->toDateString())
+                                    ->whereNull('sub_branch_terms.end_date');
+                            });
+                    })
+                    ->select(
+                        'members.id',
+                        'members.kh_name',
+                    )
+                    ->get();
+
+                return (object)[
+                    'id' => $committee->id,
+                    'kh_name' => $committee->kh_name,
+                    'members' => $members,
+                ];
+            });
+
+        return view('hq-report.hq-report-honorary-committee', [
+            'committees' => $test
+        ]);
     }
 
     public function committeeReport()
--- resources/views/hq-report/hq-report-honorary-committee.blade.php
+++ resources/views/hq-report/hq-report-honorary-committee.blade.php
@@ -5,7 +5,51 @@
 @endsection
 
 @section('content')
+    <div class="card">
+        <div class="card-header">
+            <button id="btn-print" class="btn btn-primary float-right"><i class="fa fa-print"></i> បោះពុម្ភ</button>
+        </div>
+    </div>
     <div class="container">
+        <div class="card" id="printable-area">
+            <div class="card-body">
+                <div>
+                    <div class="row">
+                        <div class="col-4"></div>
+                        <div class="col-4 text-center">
+                            <img src="{{ asset('Cambodian_Red_Cross_Logo.png') }}" alt="crc-logo"
+                                style="width: 120px; height: 120px;" class="mb-3">
+                            <div class="khm">កាកបាទក្រហមកម្ពុជា</div>
+                        </div>
+                        <div class="col-4"></div>
+                    </div>
+                    <div class="row my-3">
+                        <div class="col-12 text-center">
+                            <div class="khm">
+                                បញ្ជីររាយនាមមន្រ្តីប្រតិបត្តិស្នាក់ការកណ្តាលកាកបាទក្រហមកម្ពុជា</div>
+                        </div>
+                    </div>
+                </div>
+                <table class="table table-sm table-bordered cell-center">
+                    <thead>
+                        <tr>
+                            <th class="khm text-center font-weight-normal" colspan="1">ល.រ</th>
+                            <th class="khm text-center font-weight-normal">គោត្តនាម-នាម</th>
+                            <th class="khm text-center font-weight-normal" colspan="1">ភេទ</th>
+                            <th class="khm text-center font-weight-normal">តួនាទី ក្នុងកក្រក</th>
+                            <th class="khm text-center font-weight-normal">លេខទូរស័ព្ទ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($committees as $committee)
+                            <tr>
+                                <td class="khm" colspan="6">{{ $committee->kh_name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -16,6 +60,10 @@
             $("#hq-report>a").addClass("active");
             $("#hq-report").addClass("menu-open");
             $("#hq-honorary-committee-report>a").addClass("active");
+
+            $('#btn-print').click(function() {
+                window.print();
+            })
         });
     </script>
 @endsection
