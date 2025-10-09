e91334ca8d67a93752db018fe1f2b9b3494d3ece
Sodara Sou <sodarasou168@gmail.com>
2025-10-09 23:12:19 +0700

    minor change

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -24,6 +24,7 @@ class Member extends Model
         'ad_district_id',
         'ad_commune_id',
         'ad_village_id',
+        'member_position_order',
         'active',
         'created_by',
         'updated_by',
---
+++ resources/views/branch-report/branch-report-honorary-committee.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    របាយការណ៌គណ:កិត្តិយសថ្នាក់កណ្តាល
+@endsection
+
+@section('content')
+
+@endsection
