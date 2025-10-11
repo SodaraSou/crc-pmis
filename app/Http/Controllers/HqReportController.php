05d8cead37117f1e48fe706380ef82996f211bb4
sodarasou <sodarasou168@gmail.com>
2025-10-11 17:50:11 +0700

    fix: change the committee member query

--- app/Http/Controllers/HqReportController.php
+++ app/Http/Controllers/HqReportController.php
@@ -126,52 +126,41 @@ class HqReportController extends Controller
 
     public function committeeReport()
     {
-        $committees = DB::table('committees')
-            ->where('committee_type_id', 2)
-            ->select('id', 'kh_name')
-            ->get()
-            ->map(function ($committee) {
-                $members = DB::table('committee_member')
-                    ->join('members', 'members.id', '=', 'committee_member.member_id')
-                    ->leftJoin('branch_terms', 'branch_terms.id', '=', 'committee_member.branch_term_id')
-                    ->leftJoin('sub_branch_terms', 'sub_branch_terms.id', '=', 'committee_member.sub_branch_term_id')
-                    ->leftJoin('genders', 'genders.id', '=', 'members.gender_id')
-                    ->leftJoin('committee_positions', 'committee_positions.id', '=', 'committee_member.committee_position_id')
-                    ->where('committee_member.committee_id', $committee->id)
-                    ->where('committee_member.active', true)
-                    ->where('members.active', true)
-                    ->where(function ($query) {
-                        $query->where(function ($q) {
-                            $q->where('branch_terms.active', true)
-                                ->where('branch_terms.start_date', '<=', now()->toDateString())
-                                ->whereNull('branch_terms.end_date');
-                        })
-                            ->orWhere(function ($q) {
-                                $q->where('sub_branch_terms.active', true)
-                                    ->where('sub_branch_terms.start_date', '<=', now()->toDateString())
-                                    ->whereNull('sub_branch_terms.end_date');
-                            });
-                    })
-                    ->select(
-                        'members.id',
-                        'members.kh_name',
-                        'genders.kh_abbr as gender_abbr',
-                        'committee_member.gov_position as gov_position',
-                        'committee_positions.kh_name as committee_position',
-                        'branch_terms.kh_name as branch_term',
-                        'sub_branch_terms.kh_name as sub_branch_term'
-                    )
-                    ->get();
-
-                return (object)[
-                    'id' => $committee->id,
-                    'kh_name' => $committee->kh_name,
-                    'members' => $members,
-                ];
-            });
+        $members = DB::table('members')
+            ->leftJoin('genders', 'genders.id', '=', 'members.gender_id')
+            ->leftJoin('committee_member', 'committee_member.member_id', '=', 'members.id')
+            ->leftJoin('committee_positions', 'committee_positions.id', '=', 'committee_member.committee_position_id')
+            ->leftJoin('committees', 'committees.id', '=', 'committee_member.committee_id')
+            ->leftJoin('branch_terms', 'branch_terms.id', '=', 'committee_member.branch_term_id')
+            ->leftJoin('sub_branch_terms', 'sub_branch_terms.id', '=', 'committee_member.sub_branch_term_id')
+            ->where('members.active', true)
+            ->where('committee_member.active', true)
+            ->where('committees.branch_id', 0)
+            ->where('committees.committee_type_id', 2)
+            ->where(function ($query) {
+                $query->where(function ($q) {
+                    $q->where('branch_terms.active', true)
+                        ->where('branch_terms.start_date', '<=', now()->toDateString())
+                        ->whereNull('branch_terms.end_date');
+                })
+                    ->orWhere(function ($q) {
+                        $q->where('sub_branch_terms.active', true)
+                            ->where('sub_branch_terms.start_date', '<=', now()->toDateString())
+                            ->whereNull('sub_branch_terms.end_date');
+                    });
+            })
+            ->select(
+                'members.kh_name as member_name',
+                'genders.kh_abbr as gender',
+                'committee_member.gov_position as gov_position',
+                'committee_positions.kh_name as committee_position',
+                'branch_terms.kh_name as branch_term',
+                'sub_branch_terms.kh_name as sub_branch_term'
+            )
+            ->get();
 
         return view('hq-report.hq-report-committee', [
-            'committees' => $committees
+            'members' => $members
         ]);
     }
 }
